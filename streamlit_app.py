7445d1b546842b3d57e6af98557171798b7e6173
zakman2112 <144280863+zakman2112@users.noreply.github.com>
2023-10-22 21:26:08 -0500

    Update streamlit_app.py
    normalize using pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
+# write your own comment -uses pandas to format response 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+
